6bfc1bd869bb7117177c0328647a418d1aaa38dc
aaazzzlll <329446892@qq.com>
2026-03-03 14:22:48 +0800

    删除项目无关文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -94,14 +94,6 @@ const router = createRouter({
       name: '空间分析',
       component: SpaceAnalyzePage,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
   ],
 })
 
